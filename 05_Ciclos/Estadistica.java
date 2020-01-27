d1211c1972320e39e81da6fd2858a8b807b3a423
Bidkar Aragón Cárdenas <bidkar.aragon@udo.mx>
2020-01-27 08:09:33 -0700

    Generar números aleatorios

--- 05_Ciclos/Estadistica.java
+++ 05_Ciclos/Estadistica.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /**
  * Trabajo extraclase: Modifica el programa anterior para que los valores sean
  * ingresados por el teclado de tal forma que si dan un 23, se redondee a 20, un
@@ -7,7 +9,7 @@
  * los valores de 10 hasta el 100, ya que inicia desde 0
  * 
  * Modifica el ejercicio para que los valores sean generados aleatoriamente
- * Math.random()
+ * Random class
  * 
  * Agregar un método que determine el promedio de los valores dentro del vector
  * Un método para que devuelva el máximo
@@ -16,7 +18,7 @@
  * Otro que calcule la moda
  */
 public class Estadistica {
-    int vector[] = { 100, 80, 100, 70, 50, 50, 80, 80, 70, 80 };
+    int vector[] = new int[10];
     int fre[] = new int[10];
 
     void frecuencia() {
@@ -30,8 +32,15 @@ public class Estadistica {
             System.out.println((i + 1) * 10 + " Frecuencia " + fre[i]);
     }
 
+    void generarNumerosAleatorios() {
+        for (int i = 0; i < vector.length; i++) {
+            vector[i] = (new Random().nextInt(9) + 1) * 10;
+        }
+    }
+
     public static void main(String[] args) {
         Estadistica obj = new Estadistica();
+        obj.generarNumerosAleatorios();
         obj.frecuencia();
         obj.despliega();
     }
